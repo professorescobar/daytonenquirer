587405055986dbec6d8eada7bd848526cc6244d8
Chase Cranmer <chasecranmer@MacBookAir.lan>
2026-02-15 21:43:20 -0500

    Take reds RSS feed out of the sports section api

--- api/sports-news.js
+++ api/sports-news.js
@@ -19,7 +19,6 @@ module.exports = async (req, res) => {
     const feeds = [
       { name: "UD Flyers Athletics", url: "https://daytonflyers.com/rss.aspx?path=general" },
       { name: "UD Flyers Athletics – Men’s Basketball", url: "https://daytonflyers.com/rss.aspx?path=mbball" },
-      { name: "Cincinnati Reds", url: "https://www.mlb.com/reds/feeds/news/rss.xml" },
       { name: "Buckeye Sports Bulletin", url: "https://buckeyesports.com/feed/" },
       { name: "NBC4 Columbus Sports", url: "https://www.nbc4i.com/sports/feed/" },
       { name: "Wright State Athletics – General", url: "https://wsuraiders.com/rss?path=general" },
